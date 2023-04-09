1a5bc267c69db7eb0b9a388e56b3c6a75eba00c5
Thiago Souza dos Santos <115598075+thiagoSouza1989@users.noreply.github.com>
2023-04-09 16:29:17 -0300

    desafio08 ex carrossel

---
+++ desafios/desafio08/script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.carousel', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination'
+    }
+})
