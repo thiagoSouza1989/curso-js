673d7dbf588b537cbfae81022d32026038443e2a
Thiago Souza dos Santos <115598075+thiagoSouza1989@users.noreply.github.com>
2023-05-02 19:21:20 -0300

    aula16 funções

---
+++ aula16/funcao01.js
@@ -0,0 +1,11 @@
+function parimpar(n){
+    if (n % 2 == 0){
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+console.log(parimpar(22))
+//let res  = parimpar(5)
+//console.log(res)
