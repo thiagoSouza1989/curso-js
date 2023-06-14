23495aad595d85fa3caa3214526265f2ff1c5e00
Thiago Souza dos Santos <115598075+thiagoSouza1989@users.noreply.github.com>
2023-06-14 18:44:41 -0300

    função While

---
+++ ex16/ex05/main.js
@@ -0,0 +1,11 @@
+var contador = 1
+        var limite = 100
+
+        while (contador <= limite) {
+            console.log(contador + 'Thiago')
+
+            contador = contador + 5
+        }
+
+        console.log('Fim da execução')
+        // executar com F8
